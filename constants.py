c1ba9fd28f8678561498b83a563d166fa0b7397e
Benjamin Simonis <10655564+glomran@users.noreply.github.com>
2018-07-20 14:20:38 +0200

    Started with DB methods

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
-# API informations
+# API constants
 URL_API = "/api"
 URL_TRANSFER = URL_API + "/transfer"
 URL_INGEST = URL_API + "/ingest"
 
-# General path informations
+# General path constants
 WORKING_PATH = "/var/archivematica"
 HANDLER_PATH = WORKING_PATH + "/Archivematica-API-Handler/"
 SOURCE_PATH = WORKING_PATH + "/source"
@@ -12,7 +12,7 @@ RETRO_SOURCE_PATH = SOURCE_PATH + "/retro"
 FREIDOK_SOURCE_PATH = SOURCE_PATH + "/freidok"
 DONE_SOURCE_PATH = SOURCE_PATH + "/done"
 
-# Processing path informations
+# Processing path constants
 PROCESS_PATH = WORKING_PATH + "/sharedDirectory/sharedMicroServiceTasksConfigs/processingMCPConfigs/"
 PROCESS_DEFAULT = "defaultProcessingMCP.xml"
 PROCESS_AUTOMATED = "automatedProcessingMCP.xml"
@@ -20,18 +20,25 @@ PROCESS_PATH_AUTOMATED = PROCESS_PATH + PROCESS_AUTOMATED
 PROCESS_PATH_DEFAULT = PROCESS_PATH + PROCESS_DEFAULT
 LOCAL_PROCESS_DIR = "processingConfs/"
 
-# Database informations
+# Database constants
 DB_FILE = HANDLER_PATH + "storage.db"
 
 CREATE_TRANSFER_TABLE = "CREATE TABLE transfer (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\
-                         FOREIGN KEY (source) REFERENCES sources(_id), tname TEXT NOT NULL, \
+                         source INTEGER NOT NULL, tname TEXT NOT NULL, \
                          acnumber INTEGER, uuid TEXT, status TEXT, failcount INTEGER DEFAULT 0,\
-                         deletedate INTEGER, procconf TEXT);"
+                         deletedate INTEGER, procconf TEXT NOT NULL, \
+                         FOREIGN KEY (source) REFERENCES sources(_id));"
 
 CREATE_SOURCE_TABLE = "CREATE TABLE sources (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
                        oname TEXT NOT NULL, transfer_started INTEGER DEFAULT 0);"
 
-DELETE_ENTRY = "DELETE FROM transfer WHERE _id = (?);"
-INSERT_ENTRY = "INSERT INTO transfer VALUES (?,?,?,?,?,?,?,?);"
+DELETE_ENTRY = "DELETE FROM transfer WHERE _id = ?;"
+INSERT_ENTRY = "INSERT INTO transfer (source,tname,acnumber,uuid,status,deletedate,procconf)\
+                VALUES (?,?,?,?,?,?,?);"
 ALL_ENTRIES = "SELECT * FROM transfer;"
-ONE_ENTRY_UUID = "SELECT * FROM transfer WHERE uuid = (?);"
+ONE_ENTRY_UUID = "SELECT * FROM transfer WHERE uuid = ?;"
+
+DELETE_SOURCE = "DELETE FROM sources WHERE _id = ?;"
+INSERT_SOURCE = "INSERT INTO sources (oname) VALUES (?);"
+ALL_SOURCES = "SELECT * FROM sources;"
+ONE_SOURCE_NAME = "SELECT * FROM sources WHERE oname = ?;"
--- dbHandler.py
+++ dbHandler.py
@@ -1,19 +1,108 @@
 from constants import DB_FILE, CREATE_TRANSFER_TABLE, CREATE_SOURCE_TABLE, \
-    DELETE_ENTRY, INSERT_ENTRY, ALL_ENTRIES, ONE_ENTRY_UUID
+    DELETE_ENTRY, INSERT_ENTRY, ALL_ENTRIES, ONE_ENTRY_UUID, ALL_SOURCES, \
+    ONE_SOURCE_NAME, DELETE_SOURCE, INSERT_SOURCE
+import os.path
+import sqlite3
+
+
+def init():
+    db_exists()
+    return
+
+
+# Methods for the database
+
+def create_db_connection():
+    conn = sqlite3.connect(DB_FILE)
+    return conn
 
 
 def db_exists():
-    if True:
-        get_transfer_list()
-    else:
+    if not os.path.isfile(DB_FILE):
         create_db()
-    pass
+    return
 
 
 def create_db():
-    # Create DB + Table
-    pass
+    conn = create_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(CREATE_SOURCE_TABLE)
+    cursor.execute(CREATE_TRANSFER_TABLE)
+    conn.close()
+    return
 
+
+# Methods for the transfer table
+# TODO: Check, if the entry exists
 
 def get_transfer_list():
-    pass
+    conn = create_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(ALL_ENTRIES)
+    t_list = cursor.fetchall()
+    conn.close()
+    return t_list
+
+
+def get_transfer(uuid):
+    conn = create_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(ONE_ENTRY_UUID, uuid)
+    transfer = cursor.fetchone()
+    conn.close()
+    return transfer
+
+
+def delete_transfer(transfer_id):
+    conn = create_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(DELETE_ENTRY, transfer_id)
+    conn.close()
+    return
+
+
+# params must be list with 7 values
+# TODO: make it more complex with values from source table, update source table "transfer_started" etc
+def insert_transfer(params):
+    conn = create_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(INSERT_ENTRY, params)
+    conn.close()
+    return
+
+
+# Methods for the source table
+
+def get_source_list():
+    conn = create_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(ALL_SOURCES)
+    s_list = cursor.fetchall()
+    conn.close()
+    return s_list
+
+
+# TODO: Check for better ways to get one source entry
+def get_source(oname):
+    conn = create_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(ONE_SOURCE_NAME, oname)
+    source = cursor.fetchone()
+    conn.close()
+    return source
+
+
+def delete_source(source_id):
+    conn = create_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(DELETE_SOURCE, source_id)
+    conn.close()
+    return
+
+
+def insert_source(oname):
+    conn = create_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(INSERT_SOURCE, oname)
+    conn.close()
+    return
